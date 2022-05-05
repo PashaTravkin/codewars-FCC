eb53b07c6fec43a3371657e8851f45579f7f3811
PashaTravkin <refueller2022@bk.ru>
2022-05-05 07:13:28 +0300

    delete numbers from string

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 let myStr = "This is the first sentence. ";
 myStr += "This is the second sentence."
+
+
+function stringClean(s) {
+    let str = s;
+    str = str.replace(/[0-9]/g, ''); // удаление цыфр из строки
+    return str;
+}
